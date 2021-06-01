eb316246a24819f648e9409a2423be922201fe4d
kyl-dc <57549207+kyl-dc@users.noreply.github.com>
2021-06-01 15:35:45 -0500

    Test push, added comment

--- app/src/main/java/comp3350/studentlifesimulator/application/MainActivity.java
+++ app/src/main/java/comp3350/studentlifesimulator/application/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // added comment, just trying out a push
         setContentView(R.layout.activity_main);
     }
 }
