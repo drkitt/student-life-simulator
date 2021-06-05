f12188c0c09eb85ccb126ba853d1e441d0f3d418
kyl-dc <57549207+kyl-dc@users.noreply.github.com>
2021-06-04 20:58:34 -0500

    Created basic interaction class with name and cost value

---
+++ app/src/test/java/comp3350/studentlifesimulator/tests/objects/TestInteraction.java
@@ -0,0 +1,34 @@
+package comp3350.studentlifesimulator.tests.objects;
+
+import comp3350.studentlifesimulator.objects.Interaction;
+
+import static org.junit.Assert.*;
+
+public class TestInteraction {
+    public void testValidInteractions() {
+        Interaction study = new Interaction("Study");
+        assertEquals("Study", study.getInteractionName());
+        assertEquals(1, study.getValue());
+
+        Interaction exercise = new Interaction("Exercise", 2);
+        assertEquals("Exercise", exercise.getInteractionName());
+        assertEquals(2, exercise.getValue());
+    }
+
+    public void testInvalidInteractionValues() {
+        assertThrows(
+                IllegalArgumentException.class,
+                ()->new Interaction("InvalidAction", 0)
+        );
+
+        assertThrows(
+                IllegalArgumentException.class,
+                ()->new Interaction("InvalidAction", -1)
+        );
+
+        assertThrows(
+                IllegalArgumentException.class,
+                ()->new Interaction("InvalidAction", 10)
+        );
+    }
+}
