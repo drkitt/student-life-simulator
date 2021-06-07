9db46661499c89f5b03fc81202e7a5b44a5c5c54
Alex Kitt <alex.kitt@outlook.com>
2021-06-06 19:25:51 -0500

    Added energy bar to student

--- app/src/main/java/comp3350/studentlifesimulator/objects/Student.java
+++ app/src/main/java/comp3350/studentlifesimulator/objects/Student.java
@@ -2,13 +2,16 @@ package comp3350.studentlifesimulator.objects;
 
 public class Student
 {
+    private final int STARTING_ENERGY = 10;
     private String studentID;
     private String studentName;
+    private EnergyBar energyBar;
 
     public Student(String newStudentName)
     {
         studentID = null;
         studentName = newStudentName;
+        energyBar = new EnergyBar(STARTING_ENERGY, STARTING_ENERGY);
     }
 
     public Student(String newStudentID,String newStudentName)
