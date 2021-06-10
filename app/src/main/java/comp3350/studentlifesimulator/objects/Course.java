45c324ef172ae49cdf82a3a57d0ae8ebee5295e8
komeegbedi <oghenekomeegbedi222@gmail.com>
2021-06-10 00:10:06 -0500

    Added tests for course object and modified course object to check for specific case

--- app/src/main/java/comp3350/studentlifesimulator/objects/Course.java
+++ app/src/main/java/comp3350/studentlifesimulator/objects/Course.java
@@ -1,5 +1,6 @@
 package comp3350.studentlifesimulator.objects;
 
+
 public class Course {
 
     private final String courseID;
@@ -15,18 +16,22 @@ public class Course {
     }
 
     public String getCourseName() {
-
         return courseName;
     }
 
     public String toString() {
-
         return courseID + " - "+ courseName;
     }
 
     public boolean equals(Course other) {
         boolean result = false;
         if(other != null) {
+
+            if(other.courseID == null || other.courseName == null ||
+                courseID == null || courseName == null){
+                throw new NullPointerException("Invalid course argument passed in");
+            }
+
             result = other.courseID.equals(courseID) && other.courseName.equals(courseName);
         }
         return result;
--- app/src/test/java/comp3350/studentlifesimulator/tests/objects/TestCourses.java
+++ app/src/test/java/comp3350/studentlifesimulator/tests/objects/TestCourses.java
@@ -1,4 +1,39 @@
 package comp3350.studentlifesimulator.tests.objects;
+import comp3350.studentlifesimulator.objects.Course;
 
+import org.junit.Test;
+import static org.junit.Assert.*;
 public class TestCourses {
+
+    @Test
+    public void testTypicalCases(){
+        Course newCourse = new Course("3350" , "Software Engineering");
+        assertEquals("3350" , newCourse.getCourseID());
+        assertEquals("Software Engineering" , newCourse.getCourseName());
+        assertFalse(newCourse.equals(new Course("3250", "Software Development")));
+        assertTrue(newCourse.equals(new Course("3350", "Software Engineering")));
+    }
+
+    @Test
+    public void testNull(){
+        Course nullCourse = new Course(null , null);
+        assertNull(nullCourse.getCourseID());
+        assertNull(nullCourse.getCourseName());
+        try {
+            nullCourse.equals(new Course(null , null));
+            nullCourse.equals(new Course("", ""));
+           fail("Null Pointer Exception expected");
+        }catch (NullPointerException e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Test
+    public void testEdgeCases(){
+        Course invalidCourse = new Course("" , "");
+        assertEquals("" , invalidCourse.getCourseName());
+        assertEquals("" , invalidCourse.getCourseName());
+        assertFalse(invalidCourse.equals(new Course(" " , "")));
+        assertTrue(invalidCourse.equals(new Course("" , "")));
+    }
 }
