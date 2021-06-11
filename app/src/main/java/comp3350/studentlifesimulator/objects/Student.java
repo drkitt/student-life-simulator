3cd2267dbb129102b8ee094af7c96b1400e7fccb
kyl-dc <57549207+kyl-dc@users.noreply.github.com>
2021-06-11 09:39:50 -0500

    Adjusting brackets, and spacing as per coding standards

--- app/src/main/java/comp3350/studentlifesimulator/objects/Student.java
+++ app/src/main/java/comp3350/studentlifesimulator/objects/Student.java
@@ -1,21 +1,18 @@
 package comp3350.studentlifesimulator.objects;
 
-public class Student
-{
+public class Student {
     private static final int MAX_ENERGY = 10;
     private String studentID;
     private String studentName;
     private EnergyBar energyBar;
 
-    public Student(String newStudentName)
-    {
+    public Student(String newStudentName) {
         studentID = null;
         studentName = newStudentName;
         energyBar = new EnergyBar(MAX_ENERGY, MAX_ENERGY);
     }
 
-    public Student(String newStudentID,String newStudentName)
-    {
+    public Student(String newStudentID,String newStudentName) {
         studentID = newStudentID;
         studentName = newStudentName;
         energyBar = new EnergyBar(MAX_ENERGY, MAX_ENERGY);
