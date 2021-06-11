c55af25dcf37bf0f64915a263aa853aa5487a546
CM-beynon <35342832+CM-beynon@users.noreply.github.com>
2021-06-11 13:34:30 -0500

    Edited to fit with programming standards

--- app/src/main/java/comp3350/studentlifesimulator/objects/Time.java
+++ app/src/main/java/comp3350/studentlifesimulator/objects/Time.java
@@ -1,69 +1,44 @@
 package comp3350.studentlifesimulator.objects;
 
 public class Time {
+    private final int unitsPerDay;
+    private int day;
+    private int currentUnit;
+    private int startTime;
 
-    private final int timeMaxTime;
-    private int timeUnitTime;
-    private int timeDays;
-    private int timeCurrentUnit;
-    private int timeCurrentTime;
-    private int timeStartTime;
-
-    public Time(int unitTime, int maxTime) {
-       if(unitTime != 30 && unitTime !=60){
-           throw new IllegalArgumentException("unit time must be 30 or 60 minutes");
-       }
-
-       if(maxTime <= 0 || maxTime%30 != 0 || maxTime>480){
-           throw new IllegalArgumentException("max time must be positive, less than 480 minutes and multiple of 30");
-       }
-       this.timeMaxTime = maxTime;
-       this.timeUnitTime = unitTime;
-       this.timeDays = 1;
-       this.timeCurrentUnit = 0;
-       this.timeCurrentTime = 0;
-       this.timeStartTime = 9;
+    public Time(int unitsPerDay) {
+       this.unitsPerDay = unitsPerDay;
+       this.day = 1;
+       this.currentUnit = 0;
+       this.startTime = 9;
     }
 
-    public int getTimeDays() {
-        return timeDays;
+    public int getDays() {
+        return day;
     }
 
-    public int getTimeUnitTime() {
-        return timeUnitTime;
+    public int getTimePerDay() {
+        return unitsPerDay;
     }
 
-    public int getTimeMaxTime() {
-        return timeMaxTime;
+    public int getCurrentTime() {
+        return currentUnit;
     }
 
-    public int getTimeCurrentUnit() {
-        return timeCurrentUnit;
+    public int getStartTime() {
+        return startTime;
     }
 
-    public int getTimeCurrentTime() {
-        return timeCurrentTime;
-    }
-
-    public int getTimeStartTime() {
-        return timeStartTime;
-    }
-
-    public int timeUnitIncrement(){
-
-        int timeMaxUnit = timeMaxTime/timeUnitTime;
-
-        if(timeCurrentUnit+1<timeMaxUnit){
-
-            timeCurrentUnit = timeCurrentUnit+1;
-            timeCurrentTime = timeCurrentTime + timeUnitTime;
-
-        }else if(timeCurrentUnit+1==timeMaxUnit){
-            timeDays++;
-            timeCurrentUnit = 0;
-            timeCurrentTime = 0;
+    public int incrementTime() {
+        if (currentUnit + 1 < unitsPerDay) {
+            currentUnit++;
         }
-        return timeCurrentUnit;
+        else if (currentUnit + 1 == unitsPerDay) {
+            day++;
+            currentUnit = 0;
+        }
+
+        return currentUnit;
     }
 
 }
