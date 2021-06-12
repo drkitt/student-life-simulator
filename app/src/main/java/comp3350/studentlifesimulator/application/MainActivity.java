7cb50eae02ead9e7c14959f8a8a629bd93bde789
CM-beynon <35342832+CM-beynon@users.noreply.github.com>
2021-06-12 13:38:58 -0500

    Created basic start screen with activity switching

--- app/src/main/java/comp3350/studentlifesimulator/application/MainActivity.java
+++ app/src/main/java/comp3350/studentlifesimulator/application/MainActivity.java
@@ -3,15 +3,28 @@ package comp3350.studentlifesimulator.application;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.content.Intent;
 
 import com.example.studentlifesimulator.R;
 
+import comp3350.studentlifesimulator.presentation.CoursesActivity;
+
 public class MainActivity extends AppCompatActivity {
+    Button newGame;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        newGame = findViewById(R.id.newGameButton);
+        newGame.setOnClickListener(view -> switchActivity());
+    }
+
+    private void switchActivity() {
+        Intent coursesActivity = new Intent(this, CoursesActivity.class);
+
+        startActivity(coursesActivity);
     }
 }
