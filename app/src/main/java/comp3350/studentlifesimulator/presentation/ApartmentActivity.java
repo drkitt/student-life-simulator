3a0bd3c58bfbc3a5505b468cefb1890cfac21a87
Alex Kitt <alex.kitt@outlook.com>
2021-06-13 22:14:32 -0500

    Registered activity as the product of a bit more trial and error

---
+++ app/src/main/java/comp3350/studentlifesimulator/presentation/ApartmentActivity.java
@@ -0,0 +1,19 @@
+package comp3350.studentlifesimulator.presentation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.studentlifesimulator.R;
+
+public class ApartmentActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.apartment_activity);
+
+        TextView currentTime = findViewById(R.id.currentTime);
+        currentTime.setText("TODO get the time class in this branch!");
+    }
+}
