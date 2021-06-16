4a3b332661354d4e1c7ec5968efe6c57ab6d7c3c
Alex Kitt <alex.kitt@outlook.com>
2021-06-15 21:12:38 -0500

    Made time display update with studying actions

--- app/src/test/java/comp3350/studentlifesimulator/tests/business/TestStudentPerformingActions.java
+++ app/src/test/java/comp3350/studentlifesimulator/tests/business/TestStudentPerformingActions.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import comp3350.studentlifesimulator.business.StudentPerformingActions;
 import comp3350.studentlifesimulator.objects.Action;
 import comp3350.studentlifesimulator.objects.Student;
+import comp3350.studentlifesimulator.objects.Time;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
@@ -16,14 +17,15 @@ public class TestStudentPerformingActions {
     public void TestPerformingAction() {
         StudentPerformingActions spa = new StudentPerformingActions();
         Student student = new Student("Son Johnsmith's near-identical twin");
+        Time time = new Time(10);
 
-        Action possibleAction = new Action("Action that takes less than the student's total energy", 1, 1, student.getMaxEnergy());
-        boolean result = spa.makeStudentPerformAction(student, possibleAction);
+        Action possibleAction = new Action("Action that takes less than the student's total energy", 1, 1);
+        boolean result = spa.makeStudentPerformAction(student, possibleAction, time);
         assertTrue(result);
-        assertEquals(student.getMaxEnergy() - possibleAction.getEnergyUnit(), student.getCurrentEnergy());
+        assertEquals(Student.getMaxEnergy() - possibleAction.getEnergyUnit(), student.getCurrentEnergy());
 
-        Action impossibleAction = new Action("Action that takes more than the student's remaining energy", 10, 1, student.getMaxEnergy());
-        assertFalse(spa.makeStudentPerformAction(student, impossibleAction));
+        Action impossibleAction = new Action("Action that takes more than the student's remaining energy", 10, 1);
+        assertFalse(spa.makeStudentPerformAction(student, impossibleAction, time));
         assertThrows(
                 IllegalArgumentException.class,
                 () -> student.doAction(impossibleAction)
