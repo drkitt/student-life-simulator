4468117fb56ed150c1a72f48c36bc5b5b4393ddb
CM-beynon <35342832+CM-beynon@users.noreply.github.com>
2021-06-17 10:57:49 -0500

    Moved MainActivity to Presentation

--- app/src/main/java/comp3350/studentlifesimulator/presentation/MainActivity.java
+++ app/src/main/java/comp3350/studentlifesimulator/presentation/MainActivity.java
@@ -1,4 +1,4 @@
-package comp3350.studentlifesimulator.application;
+package comp3350.studentlifesimulator.presentation;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -7,8 +7,6 @@ import android.os.Bundle;
 import android.widget.Button;
 
 import com.example.studentlifesimulator.R;
-
-import comp3350.studentlifesimulator.presentation.CoursesActivity;
 
 public class MainActivity extends AppCompatActivity {
     Button newGame;
