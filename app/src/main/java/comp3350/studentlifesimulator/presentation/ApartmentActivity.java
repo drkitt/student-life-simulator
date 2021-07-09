2d03fc9566427432debeada61caf30581ae72d41
MrNoboddy <605883306@qq.com>
2021-07-08 19:14:38 -0500

    buttons deleted
    Delete Attend and Skip class actions, make listen to instructor button and talk to friends button invisible, organize the apartment activity buttons.

--- app/src/main/java/comp3350/studentlifesimulator/presentation/ApartmentActivity.java
+++ app/src/main/java/comp3350/studentlifesimulator/presentation/ApartmentActivity.java
@@ -3,6 +3,7 @@ package comp3350.studentlifesimulator.presentation;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
@@ -24,6 +25,7 @@ public class ApartmentActivity extends AppCompatActivity {
     private StudentPerformingActions studentPerformingActions;
     private Action marathonAction, studyAction, quickStudyAction;
     private Action hibernateAction, sleepAction, napAction;
+    private Action attendAction, listenToInstructorAction, talkToFriendsAction, skipClassAction;
     private static final int MINUTES_PER_TIME_UNIT = 15;
 
     @Override
@@ -47,6 +49,10 @@ public class ApartmentActivity extends AppCompatActivity {
         //=============== STUDY ACTIONS ==============
         //TODO: Figure our how to get remaining time or re-visit with a decision from the team
         //currently, marathon takes 1/4 of the day = 24 units out of 96 total
+
+
+
+
         marathonAction = new Action("Marathon Study", student.getCurrentEnergy(), 24, 1);
         Button marathonButton = findViewById(R.id.marathonButton);
         marathonButton.setOnClickListener(view -> doAction(marathonAction));
@@ -70,7 +76,42 @@ public class ApartmentActivity extends AppCompatActivity {
 
         napAction = new Action("Nap", 1, 1, 1);
         Button napButton = findViewById(R.id.napButton);
-        sleepButton.setOnClickListener(view -> doAction(napAction));
+        napButton.setOnClickListener(view -> doAction(napAction));
+
+        //attendAction = new Action("Attend", -1, 4, 5);
+        //Attend class buttons starts
+        Button attendButton = findViewById(R.id.attendButton);
+        Button listenButton = findViewById(R.id.listenButton);
+        Button talkToFriendsButton = findViewById(R.id.talkButton);
+        Button skipClassButton = findViewById(R.id.skipClassButton);
+
+
+        attendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                listenButton.setVisibility(v.VISIBLE);
+                talkToFriendsButton.setVisibility(v.VISIBLE);
+                attendButton.setVisibility(v.INVISIBLE);
+                skipClassButton.setVisibility(v.INVISIBLE);
+            }
+        });
+
+        listenToInstructorAction = new Action("Listen To Instructor", -2, 1, 12);
+        listenButton.setOnClickListener(view -> doAction(listenToInstructorAction));
+
+        talkToFriendsAction = new Action("Talk To Friends", -1, 1, 3);
+        talkToFriendsButton.setOnClickListener(view -> doAction(talkToFriendsAction));
+
+
+        skipClassButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                attendButton.setVisibility(v.INVISIBLE);
+                skipClassButton.setVisibility(v.INVISIBLE);
+            }
+        });
+
+
     }
 
     private void doAction(Action action) {
