6fd76499f2dd15065e0440d72577f6a0cff12175
kyl-dc <57549207+kyl-dc@users.noreply.github.com>
2021-07-08 20:28:23 -0500

    updating the ordering for the states

--- app/src/main/java/comp3350/studentlifesimulator/objects/ActionStates.java
+++ app/src/main/java/comp3350/studentlifesimulator/objects/ActionStates.java
@@ -1,9 +1,9 @@
 package comp3350.studentlifesimulator.objects;
 
 public enum ActionStates {
-    HAS_CLASS,
-    IN_CLASS_HIGH, // Energy is above 5%
     IN_CLASS_LOW,  // Energy is below 5%
+    IN_CLASS_HIGH, // Energy is above 5%
+    LOW_ENERGY,
     FREE_TIME,
-    LOW_ENERGY
+    HAS_CLASS
 }
