ff47fa2f8fad817c6d674d56f42f411d27031052
kyl-dc <57549207+kyl-dc@users.noreply.github.com>
2021-07-27 21:33:58 -0500

    FullTimeStudent acceptance test, and marking tooltip checks

--- app/src/androidTest/java/comp3350/studentlifesimulator/tests/acceptance/TimeManagementTest.java
+++ app/src/androidTest/java/comp3350/studentlifesimulator/tests/acceptance/TimeManagementTest.java
@@ -16,10 +16,9 @@ import static androidx.test.espresso.Espresso.*;
 import static androidx.test.espresso.action.ViewActions.*;
 import static androidx.test.espresso.matcher.ViewMatchers.*;
 import static androidx.test.espresso.assertion.ViewAssertions.*;
-import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.anything;
 
-//import static org.hamcrest.Matchers.not;
+import static org.hamcrest.Matchers.not;
 
 @RunWith(AndroidJUnit4.class)
 @LargeTest
@@ -37,6 +36,7 @@ public class TimeManagementTest {
         onView(withText("Select courses to be enrolled in")).check(matches(isDisplayed()));
 
         onView(withId(R.id.registerButton)).perform(click());
+        // No change in activity TODO: can we check for the tooltip?
         onView(withText("Select courses to be enrolled in")).check(matches(isDisplayed()));
 
         onData(anything()).inAdapterView(withId(R.id.courses)).atPosition(0).perform(click());
@@ -89,10 +89,123 @@ public class TimeManagementTest {
         onView(withText("Continue")).check(matches(isDisplayed()));
     }
 
-//    @Test
-//    public void testFullTimeStudent() {
-//
-//    }
+    @Test
+    public void testFullTimeStudent() {
+        onView(withId(R.id.newGameButton)).perform(click());
+        onView(withText("Customize your character")).check(matches(isDisplayed()));
+
+        onView(withText("Save")).perform(click());
+        onView(withText("Select courses to be enrolled in")).check(matches(isDisplayed()));
+
+        onData(anything()).inAdapterView(withId(R.id.courses)).atPosition(0).perform(click());
+        onData(anything()).inAdapterView(withId(R.id.courses)).atPosition(1).perform(click());
+        onData(anything()).inAdapterView(withId(R.id.courses)).atPosition(2).perform(click());
+        onData(anything()).inAdapterView(withId(R.id.courses)).atPosition(3).perform(click());
+        onData(anything()).inAdapterView(withId(R.id.courses)).atPosition(4).perform(click());
+        onView(withId(R.id.registerButton)).perform(click());
+        // No change in activity TODO: can we check for the tooltip?
+        onView(withText("Select courses to be enrolled in")).check(matches(isDisplayed()));
+        // De-selecting
+        onData(anything()).inAdapterView(withId(R.id.courses)).atPosition(0).perform(click());
+        onData(anything()).inAdapterView(withId(R.id.courses)).atPosition(1).perform(click());
+        onData(anything()).inAdapterView(withId(R.id.courses)).atPosition(2).perform(click());
+        onData(anything()).inAdapterView(withId(R.id.courses)).atPosition(3).perform(click());
+        onData(anything()).inAdapterView(withId(R.id.courses)).atPosition(4).perform(click());
+
+        onData(anything()).inAdapterView(withId(R.id.courses)).atPosition(0).perform(click());
+        onData(anything()).inAdapterView(withId(R.id.courses)).atPosition(2).perform(click());
+        onData(anything()).inAdapterView(withId(R.id.courses)).atPosition(3).perform(click());
+        onData(anything()).inAdapterView(withId(R.id.courses)).atPosition(4).perform(click());
+        onView(withId(R.id.registerButton)).perform(click());
+        onView(withText("6:00 AM\nMonday\nWeek 1")).check(matches(isDisplayed()));
+        onView(withId(R.id.energyBar)).check(matches(isDisplayed()));
+        onView(withText("Marathon Study")).check(matches(isDisplayed()));
+        onView(withText("Hibernate")).check(matches(isDisplayed()));
+
+        onView(withText("Quick Study")).perform(click());
+        onView(withText("Quick Study")).perform(click());
+        onView(withText("8:00 AM\nMonday\nWeek 1")).check(matches(isDisplayed()));
+        onView(withText("Attend")).check(matches(isDisplayed()));
+        onView(withText("Skip Class")).check(matches(isDisplayed()));
+
+        onView(withText("Attend")).perform(click());
+        onView(withText("Gossip")).check(matches(isDisplayed()));
+        onView(withText("Listen")).check(matches(isDisplayed()));
+        onView(withText("Quick Study")).check(matches(isDisplayed()));
+        onView(withText("Nap")).check(matches(isDisplayed()));
+
+        onView(withText("Listen")).perform(click());
+        onView(withText("9:00 AM\nMonday\nWeek 1")).check(matches(isDisplayed()));
+        onView(withText("Marathon Study")).check(matches(isDisplayed()));
+        onView(withText("Hibernate")).check(matches(isDisplayed()));
+
+        onView(withText("Nap")).perform(click());
+        onView(withText("10:00 AM\nMonday\nWeek 1")).check(matches(isDisplayed()));
+
+        onView(withText("Quick Study")).perform(click());
+        onView(withText("11:00 AM\nMonday\nWeek 1")).check(matches(isDisplayed()));
+        onView(withText("Attend")).check(matches(isDisplayed()));
+        onView(withText("Skip Class")).check(matches(isDisplayed()));
+
+        onView(withText("Skip Class")).perform(click());
+        onView(withText("Attend")).check(matches(not(isDisplayed())));
+        onView(withText("Skip Class")).check(matches(not(isDisplayed())));
+
+        onView(withText("Study")).perform(click());
+        onView(withText("Study")).perform(click());
+        onView(withText("7:00 PM\nMonday\nWeek 1")).check(matches(isDisplayed()));
+
+        onView(withText("Hibernate")).perform(click());
+        onView(withText("7:00 AM\nTuesday\nWeek 1")).check(matches(isDisplayed()));
+
+        onView(withText("Quick Study")).perform(click());
+        onView(withText("Quick Study")).perform(click());
+        onView(withText("Quick Study")).perform(click());
+        onView(withText("10:00 AM\nTuesday\nWeek 1")).check(matches(isDisplayed()));
+        onView(withText("Attend")).check(matches(isDisplayed()));
+        onView(withText("Skip Class")).check(matches(isDisplayed()));
+
+        onView(withText("Attend")).perform(click());
+        onView(withText("Gossip")).check(matches(isDisplayed()));
+        onView(withText("Listen")).check(matches(isDisplayed()));
+        onView(withText("Quick Study")).check(matches(isDisplayed()));
+        onView(withText("Nap")).check(matches(isDisplayed()));
+
+        onView(withText("Gossip")).perform(click());
+        onView(withText("11:00 AM\nTuesday\nWeek 1")).check(matches(isDisplayed()));
+        onView(withText("Marathon Study")).check(matches(isDisplayed()));
+        onView(withText("Hibernate")).check(matches(isDisplayed()));
+
+        onView(withText("Marathon Study")).perform(click());
+        onView(withText("7:00 PM\nTuesday\nWeek 1")).check(matches(isDisplayed()));
+
+        onView(withText("Study")).perform(click());
+        // No change in time TODO: can we check for the tooltip?
+        onView(withText("7:00 PM\nTuesday\nWeek 1")).check(matches(isDisplayed()));
+
+        onView(withText("Hibernate")).perform(click());
+        onView(withText("7:00 AM\nWednesday\nWeek 1")).check(matches(isDisplayed()));
+
+        onView(withText("Quick Study")).perform(click());
+        onView(withText("8:00 AM\nWednesday\nWeek 1")).check(matches(isDisplayed()));
+        onView(withText("Attend")).check(matches(isDisplayed()));
+        onView(withText("Skip Class")).check(matches(isDisplayed()));
+
+        onView(withText("Attend")).perform(click());
+        onView(withText("Gossip")).check(matches(isDisplayed()));
+        onView(withText("Listen")).check(matches(isDisplayed()));
+        onView(withText("Quick Study")).check(matches(isDisplayed()));
+        onView(withText("Nap")).check(matches(isDisplayed()));
+
+        onView(withText("Quick Study")).perform(click());
+        onView(withText("9:00 AM\nWednesday\nWeek 1")).check(matches(isDisplayed()));
+        onView(withText("Marathon Study")).check(matches(isDisplayed()));
+        onView(withText("Hibernate")).check(matches(isDisplayed()));
+
+        Espresso.pressBack();
+        Espresso.pressBack();
+        onView(withText("Continue")).check(matches(isDisplayed()));
+    }
 //
 //    @Test
 //    public void testSleeperSkipperStudent() {
