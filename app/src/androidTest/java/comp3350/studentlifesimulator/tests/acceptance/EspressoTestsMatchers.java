03c35fa6754874788316d1fac6dc4263db8fdbb9
CM-beynon <35342832+CM-beynon@users.noreply.github.com>
2021-07-28 19:55:24 -0500

    Update app/src/androidTest/java/comp3350/studentlifesimulator/tests/acceptance/EspressoTestsMatchers.java

--- app/src/androidTest/java/comp3350/studentlifesimulator/tests/acceptance/EspressoTestsMatchers.java
+++ app/src/androidTest/java/comp3350/studentlifesimulator/tests/acceptance/EspressoTestsMatchers.java
@@ -5,7 +5,6 @@ import android.view.View;
 import org.hamcrest.Matcher;
 
 public class EspressoTestsMatchers {
-
     public static Matcher<View> withDrawable(final int resourceId) {
         return new DrawableMatcher(resourceId);
     }
